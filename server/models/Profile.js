8a75784666ae0f9ee47b85750464134f3c10a4dd
Igor Tatarinov <igor.tanv@gmail.com>
2022-01-06 20:04:01 -0500

    add Profile Model to extend user data

---
+++ server/models/Profile.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const availabileTimes = {
+  startTime: Date,
+  endTime: Date,
+},
+
+const citySchema = new mongoose.Schema({
+  name: String,
+  location: {
+    type: {
+      type: String,
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  }
+});
+
+const profileSchema = new mongoose.Schema({
+
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 200
+  },
+  availability: [
+    {
+      type: availabileTimes
+    }
+  ],
+  location: {
+    type: citySchema
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+});
+
+
+module.exports = Profile = mongoose.model("profile", profileSchema);
